8351a80ec8246fd48955e943041e7c91aa2f45ca
GitUser <mashapacseeva@gmail.com>
2023-11-06 10:18:25 +0700

    Обработка строк

--- Programm3_lab4_Java/Main.java
+++ Programm3_lab4_Java/Main.java
@@ -5,12 +5,15 @@ public class Main{
         int test = 0;
         Scanner in = new Scanner(System.in);
         Credit credit0 = new Credit();
-        System.out.println(++test + ") Тест конструктора без параметров");
+        System.out.println(++test + ") Тест конструктора без параметров (+ демонстрация использования статического поля number - счетчика номера кредита)");
         credit0.OutData();
 
-        Credit creditTest = new Credit(123, "банк", "Сбербанк", "Иванов И. И.", 30, 20000, 0, 1000, '-', "Петров П. П.", 45000, 100000, 4.8f, 10,"рубль");
+        Credit creditTest = new Credit("банк", "Сбербанк", "Иванов И. И.", 30, 20000, 0, 1000, '-', "Петров П. П.", 45000, 100000, 4.8f, 10,"рубль");
         System.out.println("\n" + ++test + ") Тест конструктора со всеми параметрами");
         creditTest.OutData();
+
+        System.out.println("\n" + ++test + ") Тест метода для подсчета хранящихся в базе кредитов");
+        Credit.PrintCountCredits();
 
         System.out.println("\n" + ++test + ") Тест метода для проверки одобрения кредита");
         creditTest.Approve();
@@ -35,11 +38,21 @@ public class Main{
         System.out.println("\n" + ++test + ") Динамический массив объектов класса");
         ArrayList<Credit> data = new ArrayList<>();
         data.add(credit0);
-        data.add(new Credit(123, "банк", "Сбербанк", "Иванов И. И.", 30, 20000, 0, 1000, '-', "Петров П. П.", 45000, 100000, 4.8f, 10,"рубль"));
+        data.add(new Credit("банк", "Сбербанк", "Иванов И. И.", 30, 20000, 0, 1000, '-', "Петров П. П.", 45000, 100000, 4.8f, 10,"рубль"));
         data.add(new Credit(45));
         for (int i = 0; i < 3; i++)
-            System.out.println(data.get(i).GetNumber());
+            System.out.println(data.get(i).GetPeriod());
 
         data.removeAll(data);
+        
+        System.out.println("\n" + ++test + ") Обработка строк");
+        String exa = "конка";
+        String mple = "тенация";
+        System.out.println(exa + mple);
+
+        System.out.println("\n" + ++test + ") Обработка строк (string pool)");
+        String literal1 = "literal";
+        String literal2 = new String("literal").intern();
+        System.out.println(literal1.equals(literal2));
     }
 }
